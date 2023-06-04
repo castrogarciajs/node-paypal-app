da103fefc84c624eaac8e7ed16e721db886fcbf7
Sebastian009w <ia3266595@gmail.com>
2023-06-04 16:04:56 -0500

    first item #9

--- app/controllers/payment_controllers.js
+++ app/controllers/payment_controllers.js
@@ -1,4 +1,16 @@
-export function createOrder() {}
+export function createOrder() {
+  const order = {
+    intent: "CAPTURE",
+    purchase_untis: [
+      {
+        amount: {
+          currency_code: "USD",
+          value: 100.0,
+        },
+      },
+    ],
+  };
+}
 
 export function captureOrder() {}
 
