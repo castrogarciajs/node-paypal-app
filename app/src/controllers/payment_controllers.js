322697a433830c8ca47638ed3e8957fdc6ccd357
Sebastian009w <ia3266595@gmail.com>
2023-06-17 21:41:14 -0500

    Error solution 401 #18

--- app/src/controllers/payment_controllers.js
+++ app/src/controllers/payment_controllers.js
@@ -10,50 +10,51 @@ export async function createOrder(_, res) {
   try {
     const order = {
       intent: "CAPTURE",
-      purchase_untis: [
+      purchase_units: [
         {
           amount: {
             currency_code: "USD",
-            value: 100.0,
+            value: "105.70",
           },
         },
       ],
-      application_contenxt: {
-        brand_name: "My short",
+      application_context: {
+        brand_name: "mycompany.com",
         landing_page: "NO_PREFERENCE",
         user_action: "PAY_NOW",
         return_url: `${HOST}/capture-order`,
-        cancel_url: `${HOST}/cancel-order`,
+        cancel_url: `${HOST}/cancel-payment`,
       },
     };
 
     const params = new URLSearchParams();
     params.append("grant_type", "client_credentials");
 
-    const {
-      data: { access_token },
-    } = await axios.post(`${PAYPAL_API_URL}/v1/oauth2/token`, params, {
-      auth: {
-        username: PAYPAL_CLIENT,
-        password: PAYPAL_KEY,
-      },
-    });
-
-    const resp = await axios.post(
+    const { data } = await axios.post(
+      `${PAYPAL_API_URL}/v1/oauth2/token`,
+      params,
+      {
+        auth: {
+          username: PAYPAL_CLIENT,
+          password: PAYPAL_KEY,
+        },
+      }
+    );
+    const response = await axios.post(
       `${PAYPAL_API_URL}/v2/checkout/orders`,
       order,
       {
         headers: {
-          Authorization: `Bearer ${access_token}`,
+          Authorization: `Bearer ${data.access_token}`,
         },
       }
     );
 
     return res.json({
-      response: { ...resp },
+      response: response.data,
     });
   } catch (error) {
-    return res.status(401).json({ error: "Error autorizacion" });
+    return res.status(500).json({ error: "Error server" });
   }
 }
 
