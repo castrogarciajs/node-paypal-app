51e9c3fb6cb5286b86b411ecd0cad58e4b8c1f39
Sebastian009w <ia3266595@gmail.com>
2023-06-18 20:48:21 -0500

    file static #22

--- app/app.js
+++ app/app.js
@@ -2,11 +2,13 @@ import Express from "express";
 import routesPayment from "./src/routes/payment_routes.js";
 import cors from "cors";
 import morgan from "morgan";
+import path from "path";
 
 const Application = Express();
 
 Application.use(morgan("dev"));
 Application.use(cors());
+Application.use(Express.static(path.resolve("src/public")));
 Application.use(routesPayment);
 
 export default Application;
